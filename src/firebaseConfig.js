114ae49a1adc9194fbcdcd28a9469efb6de5e592
Rafael Silva <raphael_fw@hotmail.com>
2025-11-05 18:25:50 -0300

    Atualiza firebaseConfig com credenciais reais do Firebase

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,14 +3,15 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "evolufit.firebaseapp.com",
-  projectId: "evolufit",
-  storageBucket: "evolufit.appspot.com",
-  messagingSenderId: "SEU_SENDER_ID",
-  appId: "SEU_APP_ID"
+  apiKey: "AIzaSyAJDqkI-qTxyntT9gvL1fpouwAPGE21jmMI",
+  authDomain: "evolufit-54d36.firebaseapp.com",
+  projectId: "evolufit-54d36",
+  storageBucket: "evolufit-54d36.appspot.com",
+  messagingSenderId: "33284220531",
+  appId: "1:33284220531:web:553becd9c7b2d732f0ac06"
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+
