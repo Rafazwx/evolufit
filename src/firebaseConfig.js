b799722ebebc1285a61e4bd12923f733c0775566
Rafael Silva <raphael_fw@hotmail.com>
2025-11-05 18:59:05 -0300

    Corrige storageBucket para appspot.com no firebaseConfig

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,9 +1,7 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAJDqkI-qTxyntT9gvL1fpouwAPGE21jmMI",
+  apiKey: "AIzaSyAJDqkl-qTxymT9gvLlfpouwAPGE21jmMI",
   authDomain: "evolufit-54d36.firebaseapp.com",
   projectId: "evolufit-54d36",
   storageBucket: "evolufit-54d36.appspot.com",
@@ -11,7 +9,4 @@ const firebaseConfig = {
   appId: "1:33284220531:web:553becd9c7b2d732f0ac06"
 };
 
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
 
